40939692737c67593e77e00a089e187e6a45f925
KJW <kijw0003@gmail.com>
2026-03-07 02:58:12 +0900

    define 문 정의(틀만 정한거 값은 바뀔 수 있음) 및 타겟 선택 코드 틀 완료

---
+++ 260305_VGA_OV7670.srcs/sources_1/new/define.vh
@@ -0,0 +1,23 @@
+`define SQUARE 2'd0
+`define TRIANGLE 2'd1
+`define CIRCLE 2'd2
+
+`define RED 2'd0
+`define BLUE 2'd1
+`define YELLOW 2'd2
+
+`define SECTION_0 4'd0
+`define SECTION_1 4'd1
+`define SECTION_2 4'd2
+`define SECTION_3 4'd3
+`define SECTION_4 4'd4
+`define SECTION_5 4'd5
+`define SECTION_6 4'd6
+`define SECTION_7 4'd7
+`define SECTION_8 4'd8
+`define SECTION_9 4'd9
+`define SECTION_10 4'd10
+`define SECTION_11 4'd11
+`define SECTION_12 4'd12
+`define SECTION_13 4'd13
+`define SECTION_14 4'd14
--- 260305_VGA_OV7670.srcs/sources_1/new/target_select.sv
+++ 260305_VGA_OV7670.srcs/sources_1/new/target_select.sv
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
-
+`include "define.h"
 module target_select (
-    input logic       clk,
-    input logic       reset,
+    input  logic       clk,
+    input  logic       reset,
     // 모양 식별 모듈과의 signals
-    input logic [3:0] hint_count,
-    input logic [7:0] hint_data,
-    input logic       data_done,
-    input logic       frame_done
+    input  logic [3:0] hint_count,
+    input  logic [7:0] hint_data,
+    input  logic       data_done,
+    input  logic       frame_done,
     // 출력
-
+    output logic [7:0] target_data
 );
 
     // port 이름
@@ -22,64 +22,96 @@ module target_select (
 
     // frame_done : 1 프레임 안에 있는 모든 모양, 색 위치를 파악 완료하면 1 tick 생성 후에 다시 0
     //              -> hint_count 를 저장 시작
-    typedef enum {
-        IDLE,
-        DECISION
-    } state_t;
-    state_t c_state, n_state;
 
+    logic [1:0] color;
+    logic [1:0] shape;
+    logic [3:0] section;
 
-    logic [7:0] c_data_mem[0:15];
-    logic [7:0] n_data_mem[0:15];
+    assign {shape, color, section} = hint_data;
 
-    logic [5:0] n_count_reg, c_count_reg;
+    logic [7:0] n_score[0:14];
+    logic [7:0] c_score[0:14];
+    logic [7:0] c_max_score, n_max_score;
 
+    logic [3:0] c_max_index, n_max_index;
+
+    assign target_data = frame_done ? {4'b1111, c_max_index} : 8'bz; // frame_done 에 따라 출력 끊기를 안해도 되나? 생각
+
+    // 데이터 및 개수 초기화
     always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
             for (int i = 0; i < 16; i++) begin
-                c_data_mem[i] <= 0;
+                c_score[i] <= 0;
             end
-            c_count_reg <= 0;
-            c_state <= IDLE;
+            c_max_index <= 0;
+            c_max_score <= 0;
         end else begin
             for (int i = 0; i < 16; i++) begin
-                c_data_mem[i] <= n_data_mem[i];
+                c_score[i] <= n_score[i];
             end
-            c_count_reg <= n_count_reg;
-            c_state <= n_state;
+            c_max_index <= n_max_index;
+            c_max_score <= n_max_score;
         end
     end
 
+    // 데이터 저장 및 frame_done 발생 시 hint count 저장
     always_comb begin
         for (int i = 0; i < 16; i++) begin
-            n_data_mem[i] = c_data_mem[i];
+            n_score[i] = c_score[i];
         end
-        n_count_reg = c_count_reg;
-        case (c_state)
-            IDLE: begin
-                if (frame_done) begin
-                    n_state = DECISION;
-                    n_count_reg = hint_count;
-                end
-            end
-            DECISION: begin
 
-            end
-        endcase
+        if (data_done) begin
+            case (shape)  // 모양 색 위치에 맞는 score 을 더한다
+                `SQUARE: begin
+                    n_score[`SECTION_0] += 123;
+                end
+                `TRIANGLE: begin
+
+                end
+                `CIRCLE: begin
+
+                end
+            endcase
+        end
     end
 
+    always_comb begin  // score 최댓값 및 최댓값의 위치 저장
+        n_max_index = c_max_index;
+        n_max_score = c_max_score;
+        for (int i = 0; i < 16; i++) begin
+            if (c_max_score < c_score[i]) begin
+                n_max_index = i;
+                n_max_score = c_score[i];
+            end
+        end
+    end
+    // ---------------------------------------------------------------------
+    // 상태 및 우선 순위 초기화
+    // always_ff @(posedge clk or posedge reset) begin
+    //     if (reset) begin
+    //         c_state <= IDLE;
+    //         c_index_count <= 0;
+    //     end else begin
+    //         c_state <= n_state;
+    //         c_index_count <= n_index_count;
+    //     end
+    // end
 
+    // always_comb begin
+    //     n_state = c_state;
+    //     n_index_count = c_index_count;
+    //     case (c_state)
+    //         IDLE: begin
+    //             if (frame_done) begin
+    //                 n_state = CALCULATE_TARGET;
+    //             end
+    //         end
+    //         CALCULATE_TARGET: begin
 
+    //         end
+    //         SEND_TARGET: begin
 
+    //         end
+    //     endcase
+    // end
 endmodule
-
-// port 이름
-
-// [3:0] hint_count : 힌트의 개수   ( 이 신호가 들어오면 힌트의 개수가 확정됨)
-
-// [7:0] hint_data  : 힌트 데이터  모양(2 bit), 색(2 bit), 위치 (4bit) 순서대로
-
-//  data_done : 한 바이트가 완성 되었을때 1 tick 1이 되었다가 다시 끈다. -> hint_data 를 입력 받음
-
-// frame_done : 1 프레임 안에 있는 모든 모양, 색 위치를 파악 완료하면 1 tick 생성 후에 다시 0
-//              -> hint_count 를 저장 시작
